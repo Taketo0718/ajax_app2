0f82be6c0c456d9c1982c2b3dc97794d720a13dc
Taketo_1987 <68280188+gozin56@users.noreply.github.com>
2020-12-01 12:37:01 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,22 @@
 class PostsController < ApplicationController
   def index
-    
+    @posts=Post.all.order(id: 'DESC')
   end
-  def new
-    @posts=Post.all.order(created_at: 'desc')
-    @post=Post.new
-  end
+
+  # def new
+  #   @posts=Post.all.order(created_at: 'desc')
+  #   @post=Post.new
+  # end
+
   def create
     @post=Post.new
     Post.create(post_params)
-
+    redirect_to action: :index
   end
+
 private #プライベートメソッドで、ストロングパラメータを記述。
-  def post_params
+
+def post_params
     params.require(:post).permit(:content)
   end
 end
